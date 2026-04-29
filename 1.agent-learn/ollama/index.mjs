5d4cedd2838535b1e47d889ad6a3f52b934a0bb1
yanxd <yxd99324@qq.com>
2026-04-29 13:57:47 +0800

    use ollama embedding model.

--- 1.agent-learn/ollama/index.mjs
+++ 1.agent-learn/ollama/index.mjs
@@ -1,10 +1,16 @@
-import { ChatOllama } from "@langchain/ollama";
+import { ChatOllama, OllamaEmbeddings } from "@langchain/ollama";
 import chalk from "chalk";
 
 const model = new ChatOllama({
   model: "qwen3",
   baseUrl: "http://127.0.0.1:11434",
-  think: true
+  think: true,
+});
+
+const embeddings = new OllamaEmbeddings({
+  model: "qwen3-embedding",
+  baseUrl: "http://127.0.0.1:11434",
+  dimensions: 1024
 });
 
 const response = await model.invoke("你好，你是谁？");
@@ -16,3 +22,9 @@ console.log(chalk.yellow(response.additional_kwargs.reasoning_content));
 console.log(chalk.blue("🤖️ 模型输出:"));
 
 console.log(chalk.blue(response.content));
+
+const embeddingsResult = await embeddings.embedQuery("你好，你是谁？");
+
+console.log(chalk.green("LLM embeddings:"));
+
+console.log(chalk.green(embeddingsResult));
